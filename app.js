c8796ceee194f659175e0210bb4f4810d2b5b42d
Aaron Ogle <atogle@gmail.com>
2012-06-15 13:49:00 -0400

    fix port for heroku

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 var app = require('express').createServer(),
-    io = require('socket.io').listen(app);
+    io = require('socket.io').listen(app),
+    port = process.env.PORT || 5000;
 
 io.configure(function () {
   io.set("transports", ["xhr-polling"]);
   io.set("polling duration", 10);
 });
 
-app.listen(80);
+app.listen(port);
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
